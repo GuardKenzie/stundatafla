ba0a4b675fc66a714772cb6178356c2dd34f0187
Kenzie <tristanferrua@gmail.com>
2021-08-13 02:18:04 +0000

    Initial commit

---
+++ index.py
@@ -0,0 +1,72 @@
+from flask import Flask, render_template
+import tafla
+from datetime import date, datetime, time, timedelta
+import random
+
+app = Flask(__name__)
+
+day_start = "08:20"
+day_end   = "18:20"
+
+day_start = datetime.strptime(day_start, "%H:%M")
+day_end   = datetime.strptime(day_end, "%H:%M")
+
+DAYDURATION = abs(day_end - day_start).seconds
+
+background_colors = [
+    "#e0af68",
+    "#f7768e",
+    "#9ece6a",
+    "#73daca",
+    "#7dcfff",
+    "#bb9af7"
+]
+
+
+def getRuler(spacing=timedelta(minutes=30)):
+    time_strings = []
+
+    current = day_start
+    while current <= day_end:
+        time_strings.append(current.strftime("%H:%M"))
+        current += spacing
+
+    out = ["·"] * ((len(time_strings) - 1) * 3 + 1) 
+    out[0::3] = time_strings
+
+    print(out)
+
+    return out
+
+
+@app.route("/")
+def tableToday():
+    table = tafla.fetchDay(datetime.now())
+
+    return ""
+
+
+@app.route("/<int:date_delta>")
+def tableDelta(date_delta):
+    date = datetime.now() + timedelta(days=date_delta)
+    dagur = tafla.fetchDay(date)
+
+    # calculate margins
+    last_margin = 0
+    for c in dagur.classes:
+        c.top_margin = 100 * abs(day_start - c.start).seconds / DAYDURATION
+        c.top_margin -= last_margin
+
+        last_margin  = 100 * abs(day_start - c.end).seconds / DAYDURATION
+        c.height = 100 * c.duration().seconds / DAYDURATION
+    
+    context = {
+        "ruler":            getRuler(timedelta(hours=1)),
+        "dags":             date, 
+        "classes":          dagur.classes, 
+        "day_duration":     DAYDURATION,
+        "background_color": random.choice(background_colors),
+        "title":            date.strftime("%A - %d. %B")
+    }
+    
+    return render_template("day_view.html", **context)
---
+++ tafla.py
@@ -0,0 +1,114 @@
+import pandas as pd
+from datetime import datetime, time, timedelta
+import re
+
+DATEFORMAT = "%Y-%m-%d %H:%M:%S"
+TIMEPADDING = timedelta(days=20)
+
+"""
+Format:
+Dags.   Frá     til     Bygging     Staður      Hópur   Kennarar
+"""
+
+name_regex = r"(\w\w\w\d{3}\w)-\d+ (.+)$"
+
+class Timi:
+    def __init__(self, start, end, class_name, location, class_type):
+        self.start      = datetime.strptime(str(start).zfill(4), "%H%M")
+        self.end        = datetime.strptime(str(end).zfill(4),   "%H%M")
+
+        print(class_name)
+        split_name = re.findall(name_regex, class_name)[0]
+        print(split_name)
+
+        self.name       = split_name[1]
+        self.id         = split_name[0]
+
+        self.location   = location
+        self.type       = class_type
+        self.top_margin = 0
+        self.height     = 0
+
+
+    def __lt__(self, other):
+        return self.start < other.start
+    
+    def __gt__(self, other):
+        return self.start > other.start
+
+    def __le__(self, other):
+        return self.start <= other.start
+
+    def __ge__(self, other):
+        return self.start >= other.start
+
+    def duration(self) -> timedelta:
+        return abs(self.end - self.start)
+
+    def getStart(self) -> str:
+        return self.start.strftime("%H:%M")
+
+    def getEnd(self) -> str:
+        return self.end.strftime("%H:%M")
+
+class Dagur:
+    def __init__(self, classes):
+        self.classes = sorted(classes)
+    
+    def starts(self):
+        return min(self.classes).start.strftime("%H:%M")
+    
+    def ends(self):
+        return max(self.classes).end.strftime("%H:%M")
+
+
+def _parseTafla():
+    stundatafla = pd.read_excel("sheets/report.xlsx")
+    stundatafla["Dags."] = stundatafla["Dags."].apply(lambda x: datetime.strptime(x, DATEFORMAT))
+
+    return stundatafla
+
+def fetchWeek(date_to_fetch: datetime):
+    stundatafla = _parseTafla()
+
+    current_weekday = date_to_fetch.weekday()
+    start_of_day = date_to_fetch.replace(hour=0, minute=0, second=0)
+
+    # Next monday
+    days_until_next_monday = timedelta(days=(6 - current_weekday))
+    next_monday = start_of_day + days_until_next_monday
+
+    # Last monday
+    last_monday = start_of_day - timedelta(days=current_weekday)
+
+    vika = stundatafla[
+        (last_monday <= stundatafla["Dags."]) & 
+        (stundatafla["Dags."] < next_monday)
+        ]
+
+    return vika
+
+def fetchDay(date_to_fetch: datetime):
+    stundatafla = _parseTafla()
+
+    # Get day    
+    start_of_day      = date_to_fetch.replace(hour=0, minute=0, second=0)
+    start_of_next_day = date_to_fetch.replace(hour=0, minute=0, second=0) + timedelta(days=1)
+
+    dagur = stundatafla[
+        (start_of_day <= stundatafla["Dags."]) & 
+        (stundatafla["Dags."] < start_of_next_day)
+        ]
+    
+    out = []
+
+    # objectify
+    for line in dagur.itertuples():
+        out.append(Timi(line[2], line[3], line[6], line[5], "F"))
+
+    return Dagur(out)
+
+if __name__ == "__main__":
+    a = fetchDay(datetime.now() + timedelta(days=20))
+    print(a.starts())
+    print(a.ends())
